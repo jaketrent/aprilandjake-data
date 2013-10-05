0579085af5b265b5cd87b286ec4f20f8bac663a1
jaketrent <trent.jake@gmail.com>
2013-10-05 09:08:59 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AprilandjakeData::Application.config.secret_key_base = '337bfd2f926ed89508f8533b4ca00999c6daa37736a7f619cb7df960eea666d9ede9aebc63568d89c0485ae0e7b10d7765427d3116739e21ac75ee9b4f461714'
