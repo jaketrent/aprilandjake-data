a4ae54a5de5e1129d4603020abea60429ab0b322
jaketrent <trent.jake@gmail.com>
2013-10-05 13:24:43 -0600

    add basic route/ctrl

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ AprilandjakeData::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
@@ -53,4 +53,5 @@ AprilandjakeData::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root "application#index"
 end
